7c9baaaf7757cf79039d2c7f55aac0856c2f4a97
Vyacheslav Lavrikov <lavrikov2398@gmail.com>
2020-04-01 17:53:39 +0300

    Add feature to get world data on coronavirus
    Request obtain confirmed, deaths, recovered cases data which is used in the 'World' component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,29 @@ import Location from './Location';
 import World from './World';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            worldData: {
+                cases: "-",
+                deaths: "-",
+                recovered: "-"
+            }
+        }
+    }
+
+    componentDidMount() {
+        fetch("https://coronavirus-19-api.herokuapp.com/all")
+        .then(response => response.json())
+        .then(data => this.setState(() => ({worldData: data})));
+    }
+
     render() {
         return(
             <>
                 <Location localData={{}}></Location>
-                <World worldData={{}}></World>
+                <World worldData={this.state.worldData}></World>
             </>
         );
     }
